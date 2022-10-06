be2c79a84b2481f79f0fd0fe9983e5db6109ba30
Omar-Codess <omartaki8@gmail.com>
2022-10-06 16:47:20 +0200

    implementate variabili kilometri età e prezzo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // INPUT
-// Chiedere all'utente quanti kilometri vuole percorrere
-// Chiedere all'utente l'età
 
+// Chiedere all'utente quanti kilometri vuole percorrere
+const km = parseInt(prompt("Quanti kilometri vuoi percorrere?"));
+console.log(km, typeof(km));
+
+// Chiedere all'utente l'età
+const age = parseInt(prompt("Quanti anni hai?"));
+console.log(age, typeof(age));
 // ELABORAZIONE
 // calcolo prezzo del biglietto (kilometri * 0.21)
+const price = km * 0.21
+console.log(price, typeof (price));
 // SE l'età dell'utente è < 18 allora applica sconto 20%
 // ALTRIMENTI SE l'età dell'utente è > 65 allora applica sconto 40%
 
