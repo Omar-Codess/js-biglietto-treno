93ad754f4aceb71bbcccfb9612cb6b15bf9b82c9
Omar-Codess <omartaki8@gmail.com>
2022-10-06 15:47:13 +0200

    stesura pseudo codice completata

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// INPUT
+// Chiedere all'utente quanti kilometri vuole percorrere
+// Chiedere all'utente l'età
+
+// ELABORAZIONE
+// calcolo prezzo del biglietto (kilometri * 0.21)
+// SE l'età dell'utente è < 18 allora applica sconto 20%
+// ALTRIMENTI SE l'età dell'utente è > 65 allora applica sconto 40%
+
+// OUTPUT
+// comunica il prezzo finale all'utente
